2bcadaa2a8a883ed54bf421ba343f650fc206490
gashach <gashach@users.noreply.github.com>
2023-01-25 18:53:03 -0500

    create person model

---
+++ Records.Model/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Records.Model.Models;
+
+public class Person
+{
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Email { get; set; }
+    public string FavoriteColor { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+}
