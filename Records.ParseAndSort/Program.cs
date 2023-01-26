4db78bb8d28a079910a36c3944a2491e99899cd9
gashach <gashach@users.noreply.github.com>
2023-01-25 19:40:07 -0500

    add other 2 data file formats

--- Records.ParseAndSort/Program.cs
+++ Records.ParseAndSort/Program.cs
@@ -6,9 +6,18 @@ using static Records.Model.Extensions.PersonSort;
 using static Records.Model.Extensions.EnumerablePersonExtensions;
 
 var dataFile1 = "Data1.txt";
+var dataFile2 = "Data2.txt";
+var dataFile3 = "Data3.txt";
+
 
 var data1 = Parser.ParsePersons(dataFile1,"pipe");
 printData(data1);
+
+var data2 = Parser.ParsePersons(dataFile2, "comma");
+printData(data2);
+
+var data3 = Parser.ParsePersons(dataFile3, "space");
+printData(data3);
 
 
 
