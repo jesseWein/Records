e94fd92b0b87ee14a93061c6824285a66902717e
gashach <gashach@users.noreply.github.com>
2023-01-25 19:35:41 -0500

    add print data routine

--- Records.ParseAndSort/Program.cs
+++ Records.ParseAndSort/Program.cs
@@ -2,10 +2,22 @@
 
 using Records.Model.Models;
 using Records.Model.Services;
+using static Records.Model.Extensions.PersonSort;
+using static Records.Model.Extensions.EnumerablePersonExtensions;
 
 var dataFile1 = "Data1.txt";
 
 var data1 = Parser.ParsePersons(dataFile1,"pipe");
 
+printData(data1);
 
 
+void printData(IEnumerable<Person> data)
+{
+    Console.WriteLine(data.AsQueryable().SortByColorThenLastNameAscending().ListToString());
+    Console.WriteLine("----");
+    Console.WriteLine(data.AsQueryable().SortByBirthdate().ListToString());
+    Console.WriteLine("----");
+    Console.WriteLine(data.AsQueryable().SortByLastName().ListToString());
+    Console.WriteLine("***********");
+}
